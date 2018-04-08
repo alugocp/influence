309627276a30081c72cf0fb35c205364b8a30d62
alugocp <alugocp@aim.com>
2018-04-08 15:21:18 -0400

    update

--- scripts/startup.js
+++ scripts/startup.js
@@ -2,7 +2,6 @@ var site={};
 
 $(document).ready(function(){
   site.heading=$(".heading");
-  site.footer=$(".footer");
   site.content=$("#content");
 
   site.setContent("Foreign Economic Influence","home");
--- scripts/structure.js
+++ scripts/structure.js
@@ -1,14 +1,3 @@
-site.positionContent=function(){
-  site.content.offset({
-    top:site.heading.offset().top+site.heading.height()+20,
-    left:site.content.offset().left
-  });
-
-  site.footer.offset({
-    top:site.content.offset().top+site.content.height()+20,
-    left:site.footer.offset().left
-  });
-}
 site.setContent=function(name,id){
   $.get("content/"+id+".html",function(data){
     site.heading.find("h1").html(name);

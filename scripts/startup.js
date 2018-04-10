e7686c21cf57cb7d25aa07f7aa1ae25d73bbe9b8
alugocp <alugocp@aim.com>
2018-04-09 22:14:36 -0400

    update

--- scripts/startup.js
+++ scripts/startup.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
   site.heading=$(".heading");
   site.content=$("#content");
 
-  site.setContent("Foreign Economic Influence","home");
+  site.setContent("Foreign Economic Influence","citizens");
 });
